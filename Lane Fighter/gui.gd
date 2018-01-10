edc02e52c49610cf4ce5ee3de99e552157effbe7
emremaz <ersoyylmaz@gmail.com>
2018-01-10 17:47:22 +0300

    Revert "Revert "Small Improvements""
    This reverts commit 91415a7601a5b278d01664f5ee37c37879169327.

--- Lane Fighter/gui.gd
+++ Lane Fighter/gui.gd
@@ -5,10 +5,10 @@ var text1
 var text2
 
 func _ready():
-	get_node("jumps_left").set_pos(Vector2(0,0))
+	get_node("Stamina").set_pos(Vector2(10,10))
+	get_node("Resource1_outline").set_pos(Vector2(get_node("Stamina").get_size().x + 15 ,10))
+	#get_node("Resource1").edit_set_pivot(Vector2(0,0))
 	set_fixed_process(true)
 
 func _fixed_process(delta):
-	jumps_left = get_owner().get_node("dummy").jumping
-	text1 = "Jumps Left : %s" % jumps_left
-	get_node("jumps_left").set_text(text1)
+	get_node("Stamina").set_text("Stamina")
